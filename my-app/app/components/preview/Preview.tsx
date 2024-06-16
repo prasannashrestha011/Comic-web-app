273931b03fd22a8dea3b874ca5e9c20bd79700fa
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-06-16 14:26:50 +0545

    preview close button added

--- my-app/app/components/preview/Preview.tsx
+++ my-app/app/components/preview/Preview.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 
 interface PreviewProp{
@@ -34,7 +36,13 @@ const Preview: React.FC<PreviewProp> = ({name,img,author,artist,context,publised
     return (
       <div onClick={handleClose} ref={modelRef} className='fixed top-0 left-0 right-0 bottom-0  backdrop-blur-md  bg-opacity-30 flex justify-center items-center'>
             <div className='overflow-hidden rounded-lg' style={{width:'95%',height:'auto'}}> 
-            <div className='bg-red-600 flex items-center justify-center h-14 lg:text-xl sm:text-xs'>{name}</div>
+            <div className='bg-red-600 flex items-center justify-center h-14 lg:text-xl sm:text-xs'>
+                <div className='flex-1  flex justify-center'> {name}</div>
+              <button className='flex-none w-9 flex justify-center items-center rounded-full  bg-red-900 h-9' 
+                onClick={()=>setShowPreview(!showPreview)}>
+                  <FontAwesomeIcon icon={faClose}/>
+                  </button>
+              </div>
             <div className='flex flex-col justify-center items-center text-xl '>
             
              <span className=' w-full flex justify-center'>
