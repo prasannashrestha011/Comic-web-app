ea5f276d61b23fdbd4218b5501a5ce836453003c
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-06-17 18:02:14 +0545

    sorting method added

--- my-app/app/characters/page.tsx
+++ my-app/app/characters/page.tsx
@@ -1,63 +1,14 @@
-'use client'
-import React, { useState } from 'react'
-import { ListOfCharactersCard } from '../source/options'
-import PreViewCharacters from '../components/preview/PreViewCharacters'
-interface CharacterProps{
-  name:string,
-  details:string,
-  img:string,
-  type:string,
-}
-const ListOfCharacters = () => {
-  const [show_character_card,setShowCharacterCard]=useState<boolean>(false)
-  const [characterPreview,setCharacterPreview]=useState<CharacterProps | null>(null)
- 
 
-  const toggleCharacterCard=(name:string,details:string,img:string,type:string)=>{
-    setCharacterPreview({name:name,
-      details:details,
-      img:img,
-      type:type})
-    setShowCharacterCard(!show_character_card)
-  }
+import React from 'react'
+import CharacterList from '../components/DataList/CharacterList'
+
+const ListOfCharacters = () => {
+
   
   return (
     <div>
       <div className='m-auto  flex  text-2xl justify-center'>List of Characters</div>
-        <ul className='flex flex-col gap-5'>
-        {ListOfCharactersCard.map((card,idx)=>{
-        return(
-            <li key={idx}>
-                <div className="card card-side bg-base-100 shadow-xl">
-                <figure>
-               
-                    <img src={card.img} 
-                  
-                    className={`rounded-lg lg:w-52 sm:w-full  `}
-                    />
-                   
-                    </figure>
-                <div className="card-body">
-                    <h2 className="card-title w-44  lg:text-2xl">{card.name}</h2>
-                    <p></p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary" 
-                    onClick={()=>toggleCharacterCard(card.name,card.details,card.img,card.type)}>View Details</button>
-                    </div>
-                </div>
-                </div>
-            </li>
-        )
-      })}
-      {show_character_card&&characterPreview?
-      <PreViewCharacters 
-      name={characterPreview.name} 
-      details={characterPreview.details}
-      img={characterPreview.img}
-      type={characterPreview.type}
-      showPreview={()=>setShowCharacterCard(!show_character_card)}
-      />:""}
-        </ul>
+       <CharacterList/>
     </div>
   )
 }
---
+++ my-app/app/components/DataList/CharacterList.tsx
@@ -0,0 +1,73 @@
+'use client'
+import { ListOfCharactersCard } from '@/app/source/options'
+import React, { useState } from 'react'
+import PreViewCharacters from '../preview/PreViewCharacters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSort, faSortAlphaAsc, faSortAlphaUp } from '@fortawesome/free-solid-svg-icons'
+
+interface CharacterProps{
+    name:string,
+    details:string,
+    img:string,
+    type:string,
+  }
+const CharacterList = () => {
+    const [show_character_card,setShowCharacterCard]=useState<boolean>(false)
+    const [characterPreview,setCharacterPreview]=useState<CharacterProps | null>(null)
+    const [issorted,setIsSorted]=useState<boolean>(false)
+    const [sortedcard,setSortedCard]=useState<CharacterProps[]>([])
+  
+    const toggleCharacterCard=(name:string,details:string,img:string,type:string)=>{
+      setCharacterPreview({name:name,
+        details:details,
+        img:img,
+        type:type})
+      setShowCharacterCard(!show_character_card)
+    }
+    const sortCharacterByAlphabet=()=>{
+        setIsSorted(!issorted)
+     setSortedCard([...ListOfCharactersCard].sort((a,b)=>a.name.localeCompare(b.name)))
+    }
+  return (
+    <div>
+       <ul className='flex flex-col gap-5'>
+        <p onClick={sortCharacterByAlphabet}><FontAwesomeIcon icon={faSortAlphaUp}/></p>
+        {(issorted?sortedcard:ListOfCharactersCard).map((card,idx)=>
+        (
+        
+            <li key={idx}>
+                <div className="card card-side bg-base-100 shadow-xl">
+                <figure>
+               
+                    <img src={card.img} 
+                  
+                    className={`rounded-lg lg:w-52 sm:w-full  `}
+                    />
+                   
+                    </figure>
+                <div className="card-body">
+                    <h2 className="card-title w-44  lg:text-2xl">{card.name}</h2>
+                    <p></p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary" 
+                    onClick={()=>toggleCharacterCard(card.name,card.details,card.img,card.type)}>View Details</button>
+                    </div>
+                </div>
+                </div>
+            </li>
+       
+      ))}
+      {show_character_card&&characterPreview?
+      <PreViewCharacters 
+      name={characterPreview.name} 
+      details={characterPreview.details}
+      img={characterPreview.img}
+      type={characterPreview.type}
+      showPreview={()=>setShowCharacterCard(!show_character_card)}
+      />:""}
+        </ul>
+    </div>
+  )
+}
+
+export default CharacterList
