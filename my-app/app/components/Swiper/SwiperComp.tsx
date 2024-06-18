691cc0d8797a4aab746eac3f213c13ebeb95648b
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-06-18 17:06:16 +0545

    pagination added and styled on homepage slider

--- my-app/app/components/Swiper/SwiperComp.tsx
+++ my-app/app/components/Swiper/SwiperComp.tsx
@@ -4,14 +4,23 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import SwiperCore from 'swiper';
 
+import { Pagination } from 'swiper/modules';
+SwiperCore.use([Pagination]);
 const SwiperComp = () => {
   return (
     <div className="m-auto flex justify-center items-center lg:w-full  text-black">
       <Swiper
+     
+       pagination={{ clickable: true }} // Enable pagination dots
+       style={{ "--swiper-pagination-color": "white", 
+         "--swiper-pagination-bullet-inactive-color": "#999999",
+         "--swiper-pagination-bullet-inactive-opacity": "1",
+         "--swiper-pagination-bullet-size": "9px", } as any}
         className="  rounded-lg lg:w-9/12 sm:w-full h-96  "
     
-        pagination={{ clickable: true }}
+       
       >
         <SwiperSlide className="flex items-center justify-center text-center text-3xl bg-white">
             <img src="https://wallpapercave.com/wp/wp1829343.jpg"     className="h-full w-full object-cover"/>
